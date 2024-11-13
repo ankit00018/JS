039cb9b4f4278e482a945db3d419ea09d1189fa6
ankit00018 <ankitkachhad9@gmail.com>
2024-11-13 12:47:55 +0530

    Array,Object,Functions

--- test.js
+++ test.js
@@ -1,1 +1,56 @@
-console.log("Ankit");
+
+const accountId = 34343
+let employeeName = "Ankit"
+var employeeGender = "Male"
+let accountState
+
+// prefer not to use var because of issue in block scope and functional scope
+
+// console.table([employeeName, employeeGender, accountId, accountState])
+
+let score = "Ankit"   // NaN = not a number 
+let valueIn =  Number(score)
+// console.log(typeof valueIn, valueIn)
+
+// console.log(valueIn);
+
+
+                                                           // OPERATION
+
+let x = 3;
+const y = x++;
+
+// console.log(x,y);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+// console.log(a,b);
+// Expected output: "a:4, b:4"
+
+const Marks = null
+
+const MyScore = ["Physics","Chemistry","Biology"]
+
+console.log(typeof MyScore);
+
+let Score = {
+    Name : "hitesh",
+    age : 20,
+ }
+
+
+console.log(typeof Score);
+
+const MyFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof MyFunction);
+console.log(typeof Marks);
+
+
+
+
+
