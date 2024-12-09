2bb1e307c39f33028b94ef14cc2453f4f856f867
ankit00018 <ankitkachhad9@gmail.com>
2024-12-09 12:24:52 +0530

    Object Part 1

--- object.js
+++ object.js
@@ -1,3 +1,46 @@
-let name = "ankit"
-console.log(name);
+//singleton 
+
+//object literals
+
+const mysum = Symbol("key1")
+
+const JSuser = {
+    name : "Ankit",
+    email : "ankitspam05@gmail.com",
+    [mysum] : "Ankit1",
+    "full name " : "Ankit Kachhad",
+    age : 21,
+    location : "Ahmedabad"
+}
+
+// console.log(JSuser.email);
+// console.log(JSuser["email"]);
+// console.log(JSuser["full name"]);
+// console.log(JSuser[mysum]);
+
+JSuser.email = "ankitspam06@google.com"
+// Object.freeze(JSuser)
+
+// JSuser.email = "ankitspam06@youtube.com"
+
+// console.log(JSuser["email"]);
+
+
+JSuser.welcome = function(){
+    console.log("Welcome");    
+}
+
+JSuser.welcomeTwo = function(){
+    console.log(`Welcome, ${this.name}`);    
+}
+console.log(JSuser.welcome());
+console.log(JSuser.welcomeTwo());
+
+
+
+
+
+
+
+
 
