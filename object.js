842354ad07b52d1dc4cbb58aa9145dbff5e7c092
ankit00018 <ankitkachhad9@gmail.com>
2024-12-10 11:09:39 +0530

    Json Intro

--- object.js
+++ object.js
@@ -58,13 +58,37 @@ const regularUser = {
 
 // console.log(regularUser.fullname.Username.lastname);
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-const obj3 = {5: "a", 6: "b"}
+const chem = {1: "a", 2: "b"}
+const phy = {3: "c", 4: "d"}
+const math = {5: "e", 6: "f"}
 
-// const obj4 = {obj1, obj2}
+// const total = {chem, phy}
 
-const obj4 = Object.assign{{}, obj1, obj2, obj3}
+const total = Object.assign({}, chem, phy, math) // also using spread operator 
+// console.log(total);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
 
 
 
