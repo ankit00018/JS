d9a64b2597239d5a15ad8566a2cea588f72d08c4
ankit00018 <ankitkachhad9@gmail.com>
2024-12-20 21:24:22 +0530

    while and  do while

--- iteration.js
+++ iteration.js
@@ -22,11 +22,34 @@ for(let i=0; i<=10; i++){
     console.log(i);
 }
 
-for(let i=0; i<=10; i++){
+for(let i=0;i<=10; i++){
     if(i==5){
         console.log("Detected 5");
         continue
     }
-    console.log(i);
-    
+    console.log(i);   
+
 }
+ 
+// while loop
+
+
+let i=0
+while(i<=10){
+    console.log(i);
+    i++
+}
+
+const myarray = ["superman", "Batman", "flash"]
+let arr=0
+while(arr<myarray.length){
+    console.log(myarray[arr]);
+    arr++
+}
+
+score = 11
+
+do{
+    console.log(score);
+    score++
+}while(score<=10)
